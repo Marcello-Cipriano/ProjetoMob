d6d0cc6a152271a2dba5cd84394ac565f806a173
PedroHTLeal <143100655+PedroHTLeal@users.noreply.github.com>
2024-05-18 21:49:23 -0300

    Update CadastroScreen.js

--- screens/CadastroScreen.js
+++ screens/CadastroScreen.js
@@ -1,20 +1,27 @@
-import React, { useState } from 'react';
-import { View, Text, TextInput, Button, StyleSheet } from 'react-native';
+import React, { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  Button,
+  StyleSheet,
+  ImageBackground,
+} from "react-native";
 
 const CadastroScreen = ({ navigation }) => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
 
   const handleCadastro = () => {
     // Verifica se o email é válido
     if (!validateEmail(email)) {
-      alert('Por favor, insira um email válido.');
+      alert("Por favor, insira um email válido.");
       return;
     }
 
     // Lógica de cadastro
     // Navegação para a próxima tela após o cadastro bem-sucedido
-    navigation.navigate('Home');
+    navigation.navigate("Login");
   };
 
   // Função para validar o formato do email usando regex
@@ -24,45 +31,58 @@ const CadastroScreen = ({ navigation }) => {
   };
 
   return (
-    <View style={[styles.container, { backgroundColor: '#FFEFD5' }]}>
-      <Text style={styles.title}>Cadastro</Text>
-      <TextInput
-        style={styles.input}
-        placeholder="Email"
-        onChangeText={setEmail}
-        value={email}
-        keyboardType="email-address" // Define o teclado como de email
-        autoCapitalize="none" // Desativa a autocapitalização
-      />
-      <TextInput
-        style={styles.input}
-        placeholder="Password"
-        onChangeText={setPassword}
-        value={password}
-        secureTextEntry
-      />
-      <Button title="Cadastrar" onPress={handleCadastro} />
-    </View>
+    <ImageBackground
+      source={{
+        uri: "https://th.bing.com/th/id/OIG2.VLliwxyFOO72BgrDS0Mg?pid=ImgGn",
+      }}
+      style={styles.background}
+    >
+      <View style={styles.container}>
+        <Text style={styles.title}>Cadastro</Text>
+        <TextInput
+          style={styles.input}
+          placeholder="Email"
+          onChangeText={setEmail}
+          value={email}
+          keyboardType="email-address" // Define o teclado como de email
+          autoCapitalize="none" // Desativa a autocapitalização
+        />
+        <TextInput
+          style={styles.input}
+          placeholder="Password"
+          onChangeText={setPassword}
+          value={password}
+          secureTextEntry
+        />
+        <Button title="Cadastrar" onPress={handleCadastro} />
+      </View>
+    </ImageBackground>
   );
 };
 
 const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    resizeMode: "cover",
+    justifyContent: "center",
+  },
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
+    justifyContent: "center",
+    alignItems: "center",
     padding: 20,
+    backgroundColor: "rgba(255, 255, 255, 0.8)", // Adiciona uma cor de fundo com transparência
   },
   title: {
     fontSize: 24,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginBottom: 20,
   },
   input: {
-    width: '100%',
+    width: "100%",
     height: 40,
     borderWidth: 1,
-    borderColor: '#ccc',
+    borderColor: "#ccc",
     borderRadius: 5,
     paddingHorizontal: 10,
     marginBottom: 10,
